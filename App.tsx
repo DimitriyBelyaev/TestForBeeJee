57c25b2d838dc8bac53f1817b265f29c9bf01c22
Dimitriy <misterdniwe@yandex.ru>
2023-06-01 19:49:56 +0300

    test push

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+}
+export default App;
